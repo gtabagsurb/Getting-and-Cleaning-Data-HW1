befb435c949adb7af5c8f843b90938fa7e9d5e34
Timur <gta@bagsurb.ru>
2014-07-28 05:08:14 +0600

    second commit

---
+++ run_analysis.R
@@ -0,0 +1,32 @@
+library(data.table)
+library(plyr)
+
+tst=read.table("./UCI HAR Dataset/test/X_test.txt")
+trn=read.table("./UCI HAR Dataset/train/X_train.txt")
+
+#adding column names from features.txt
+features<-read.table("./UCI HAR Dataset/features.txt")
+colnames(tst)<-features[,2]
+colnames(trn)<-features[,2]
+
+#adding column with subject from subject_*.txt
+tst_sbj = read.table("./UCI HAR Dataset/test/subject_test.txt")
+tst$subject = tst_sbj[,1]
+trn_sbj = read.table("./UCI HAR Dataset/train/subject_train.txt")
+trn$subject = trn_sbj[,1]
+
+#adding activities column from y_*.txt
+tst_act = read.table("./UCI HAR Dataset/test/y_test.txt")
+trn_act = read.table("./UCI HAR Dataset/train/y_train.txt")
+activities=read.table("./UCI HAR Dataset/activity_labels.txt")
+tst$activity = factor(tst_act$V1,levels = activities$V1,labels = activities$V2)
+trn$activity = factor(trn_act$V1,levels = activities$V1,labels = activities$V2)
+
+#union of 2 data frames
+tidy_data=rbind(tst, trn)
+write.table(tidy_data, "tidy_data.txt",row.names=FALSE)
+
+names(tidy_data)
+tidy_data2 = data.frame()
+tidy_data[tidy_data$subject,tidy_data$activity]
+tidy_data$subject
